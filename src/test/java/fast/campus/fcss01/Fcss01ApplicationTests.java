2410ca945e9d47010a934315474f89bd67974d4d
Jaeuk Oh <wooko5@naver.com>
2025-06-08 18:10:15 +0900

    init project code

---
+++ src/test/java/fast/campus/fcss01/Fcss01ApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.campus.fcss01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fcss01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
