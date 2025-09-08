6be2a609cd945c877e6177a17b0b0795f8492686
Jaeuk Oh <wooko5@naver.com>
2025-09-08 23:58:11 +0900

    feat : 강의본 추가

--- src/main/java/fast/campus/fcss01/controller/TestController.java
+++ src/main/java/fast/campus/fcss01/controller/TestController.java
@@ -1,5 +1,7 @@
 package fast.campus.fcss01.controller;
 
+import fast.campus.fcss01.user.Person;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,10 @@ public class TestController {
     @GetMapping("/api/v1/hello")
     public String hello() {
         return "hello spring security";
+    }
+
+    @GetMapping("/api/v2/hello")
+    public ResponseEntity<Person> helloV2() {
+        return ResponseEntity.ok(new Person("2", 1));
     }
 }
---
+++ src/main/java/fast/campus/fcss01/user/Person.kt
@@ -0,0 +1,6 @@
+package fast.campus.fcss01.user
+
+data class Person(
+    val name: String,
+    val age: Int,
+)
